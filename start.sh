d34966a959c37afaf65af0451b5f7d10106690b4
narutooxox <112558957+narutooxox@users.noreply.github.com>
2022-09-12 00:14:07 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,15 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Lovelyjagan/Sm18filter4k.git /Sm18filter4k
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone https://github.com/Lovelyjagan/Sm18filter4k
+fi
+ls
+cd /Sm18filter4k/Sm18filter4k
+ls
+$PWD
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
